16668b84e261649da14e3fe377da3a84748e5bcf
insung151 <insung151@naver.com>
2019-02-15 14:54:47 +0900

    Update database setting

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -2,3 +2,13 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -1,3 +1,13 @@
 from .base import *
 
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
